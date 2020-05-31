6ab472c0b980fce94d4e330aef5300b2507bebf7
Marks-Ray <gabrielmc543@edu.univali.br>
2020-05-31 08:07:16 -0300

    todos os insert funcionando, faltando apenas a data e validacao da consulta

--- app/src/main/java/com/example/app_consulta/fragment/add/AddConsultaFragment.java
+++ app/src/main/java/com/example/app_consulta/fragment/add/AddConsultaFragment.java
@@ -1,5 +1,8 @@
 package com.example.app_consulta.fragment.add;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app_consulta.R;
 
@@ -14,6 +21,12 @@ import com.example.app_consulta.R;
  * A simple {@link Fragment} subclass.
  */
 public class AddConsultaFragment extends Fragment {
+
+    TextView obs;
+    Spinner nomeMedico, nomePaciente;
+    Button btn_consulta;
+
+    SQLiteDatabase db;
 
     public AddConsultaFragment() {
         // Required empty public constructor
@@ -24,6 +37,41 @@ public class AddConsultaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_consulta, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_consulta, container, false);
+
+        obs = view.findViewById(R.id.consulta_obs);
+        nomeMedico = view.findViewById(R.id.consulta_medico);
+        nomePaciente = view.findViewById(R.id.consulta_paciente);
+
+        btn_consulta = view.findViewById(R.id.btn_salvar_consulta);
+
+        btn_consulta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String observ, Medico, paciente;
+                observ = obs.getText().toString().trim();
+                Medico = nomeMedico.getSelectedItem().toString();
+                paciente = nomePaciente.getSelectedItem().toString();
+
+                if (observ.equals("")) {
+                    Toast.makeText(v.getContext(), "Todos os campos deve ser preenchidos", Toast.LENGTH_LONG).show();
+                } else {
+                    db = getActivity().getApplicationContext().openOrCreateDatabase("appConsulta.db", Context.MODE_PRIVATE, null);
+                    StringBuilder sql = new StringBuilder();
+                    sql.append("INSERT INTO paciente(nome,grp_sanguineo, crm,logradouro,numero,cidade,uf,celular,fixo) VALUES (");
+
+                    sql.append(");");
+
+                    try {
+                        db.execSQL(sql.toString());
+                        Toast.makeText(v.getContext(), "Medico inserido", Toast.LENGTH_LONG).show();
+                    } catch (SQLException e) {
+                        Toast.makeText(v.getContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        return  view;
     }
 }
--- app/src/main/java/com/example/app_consulta/fragment/add/AddPacienteFragment.java
+++ app/src/main/java/com/example/app_consulta/fragment/add/AddPacienteFragment.java
@@ -1,5 +1,8 @@
 package com.example.app_consulta.fragment.add;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app_consulta.R;
 
@@ -14,6 +21,12 @@ import com.example.app_consulta.R;
  * A simple {@link Fragment} subclass.
  */
 public class AddPacienteFragment extends Fragment {
+
+    TextView nomeP, logradouroP, cidadeP, celularP, fixoP, numeroP;
+    Spinner ufP, sangueP;
+    SQLiteDatabase db;
+
+    Button btn_salvar_paciente;
 
     public AddPacienteFragment() {
         // Required empty public constructor
@@ -24,6 +37,59 @@ public class AddPacienteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_paciente, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_paciente, container, false);
+
+        nomeP = view.findViewById(R.id.nomePaciente);
+        logradouroP = view.findViewById(R.id.logradouroPaciente);
+        cidadeP = view.findViewById(R.id.cidadePaciente);
+        celularP = view.findViewById(R.id.celularPaciente);
+        fixoP = view.findViewById(R.id.teleFixoPaciente);
+        numeroP = view.findViewById(R.id.numCasaPaciente);
+        ufP = view.findViewById(R.id.ufPaciente);
+        sangueP = view.findViewById(R.id.gprSanguineo);
+
+        btn_salvar_paciente = view.findViewById(R.id.btn_salvar_paciente);
+
+        btn_salvar_paciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome, sangue, logradouro, cidade, celular, fixo, numero, uf;
+                nome = nomeP.getText().toString().trim();
+                logradouro = logradouroP.getText().toString().trim();
+                cidade = cidadeP.getText().toString().trim();
+                celular = celularP.getText().toString().trim();
+                fixo = fixoP.getText().toString().trim();
+                numero = numeroP.getText().toString().trim();
+                uf = ufP.getSelectedItem().toString();
+                sangue = sangueP.getSelectedItem().toString();
+
+                if (nome.equals("") || logradouro.equals("") || cidade.equals("") || celular.equals("") || fixo.equals("") || numero.equals("")) {
+                    Toast.makeText(v.getContext(), "Todos os campos deve ser preenchidos", Toast.LENGTH_LONG).show();
+                } else {
+                    db = getActivity().getApplicationContext().openOrCreateDatabase("appConsulta.db", Context.MODE_PRIVATE, null);
+                    StringBuilder sql = new StringBuilder();
+                    sql.append("INSERT INTO paciente(nome,grp_sanguineo, crm,logradouro,numero,cidade,uf,celular,fixo) VALUES (");
+                    sql.append("'" + nome + "', ");
+                    sql.append("'" + sangue + "', ");
+                    sql.append("'" + logradouro + "', ");
+                    sql.append("'" + numero + "', ");
+                    sql.append("'" + cidade + "', ");
+                    sql.append("'" + uf + "', ");
+                    sql.append("'" + celular + "', ");
+                    sql.append("'" + fixo + "'");
+                    sql.append(");");
+
+                    try {
+                        db.execSQL(sql.toString());
+                        Toast.makeText(v.getContext(), "Medico inserido", Toast.LENGTH_LONG).show();
+                    } catch (SQLException e) {
+                        Toast.makeText(v.getContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+
+        return view;
     }
 }
